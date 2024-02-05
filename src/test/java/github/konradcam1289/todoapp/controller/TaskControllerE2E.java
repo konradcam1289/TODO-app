0bac864ee435fe494c595bc3c5cbcba42371b000
konradcam1289 <118669378+konradcam1289@users.noreply.github.com>
2024-02-05 20:15:09 +0100

    Update TaskControllerE2E.java

--- src/test/java/github/konradcam1289/todoapp/controller/TaskControllerE2E.java
+++ src/test/java/github/konradcam1289/todoapp/controller/TaskControllerE2E.java
@@ -29,48 +29,20 @@ class TaskControllerE2E {
 
     @Test
     void httpGet_returnAllTasks() {
-        //given
         var initial = repo.findAll().size();
         repo.save(new Task("foo", LocalDateTime.now()));
         repo.save(new Task("bar", LocalDateTime.now()));
-
-        //and
-        //when
         Task[] result = restTemplate.getForObject("http://localhost:" + port + "/tasks", Task[].class);
-
-        //then
         assertThat(result).hasSize(initial + 2);
     }
 
     @Test
     void httpGet_returnsTaskById() {
-        //given
         Task task = repo.save(new Task("test",
                 LocalDateTime.now()));
-        //when
         Task result = restTemplate.getForObject(
                 "http://localhost:" + port + "/tasks/"
                         + task.getId(), Task.class);
-        //then
         assertThat(result.getId()).isEqualTo(task.getId());
     }
-
-//    @Test
-//    void httpPost_returnsCreatedResponse() {
-//        Task task = new Task("test2", LocalDateTime.now());
-//        ResponseEntity<?> result = restTemplate.postForEntity("http://localhost:"+port+"/tasks", task, ResponseEntity.class);
-//        URI url = result.getHeaders().getLocation();
-//        String path = url.getPath();
-//        Task toCheck = restTemplate.getForObject("http://localhost:"+port+path, Task.class);
-//
-//        assertThat(result.getStatusCode())
-//                .isEqualTo(HttpStatus.CREATED);
-//        assertThat(toCheck.getDeadline())
-//                .isCloseTo(task.getDeadline(),
-//                        within(100, ChronoUnit.MILLIS));
-//        assertThat(toCheck.getDescription())
-//                .isEqualTo(task.getDescription());
-//
-//    }
-
 }
