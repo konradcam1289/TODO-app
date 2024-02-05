2f075dd4f745a8be754b2e2f76e10a2b08e0b260
konradcam1289 <118669378+konradcam1289@users.noreply.github.com>
2024-02-05 20:18:13 +0100

    Update ProjectService.java

--- src/main/java/github/konradcam1289/todoapp/logic/ProjectService.java
+++ src/main/java/github/konradcam1289/todoapp/logic/ProjectService.java
@@ -13,8 +13,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 
-//@Service
-
 public class ProjectService {
     private ProjectRepository repository;
     private TaskGroupRepository taskGroupRepository;
